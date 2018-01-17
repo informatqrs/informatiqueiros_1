4d9f5bbeb4554c29f3bf8517c5392828ed590d41
Your Name <you@example.com>
2018-01-17 00:24:02 -0300

    Jan 17 00:24

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,10 +4,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <script type="text/javascript" src="{{ asset('js/app.js')}}"></script>
     <title>@yield('titulo')</title>
   </head>
   <body>
       @yield('conteudo')
   </body>
+  <script type="text/javascript" src="{{ asset('js/app.js')}}"></script>
 </html>
--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-sm bg-dark fixed-top">
-  <ul class="navbar-menu">
+  <ul class="navbar-nav">
     <li class="nav-item"> <a class="nav-link" href="#">Link 1</a></li>
     <li class="nav-item"> <a class="nav-link" href="#">Link 1</a></li>
     <li class="nav-item"> <a class="nav-link" href="#">Link 1</a></li>
