89e5a09459f16d263757ac8a3b5e2bc9307a042c
Your Name <you@example.com>
2018-01-17 00:52:31 -0300

    Jan 17 00:52

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,51 +1,32 @@
 @extends('master')
+
 @section('conteudo')
 @include('nav')
-    <div class="">
-      <table>
-        <tr>
-          <td><h1>Loja de Produtos</h1></td><td><form class="" action="index.html" method="post">
-                  <input type="text" name="" value=""><button type="button" name="button">Ir</button>
-                </form></td>
-        </tr>
-      </table>
+<div class="container">
+  <div class="row">
+
+    <!-- MENU LATERAL -->
+
+    <div class="col bg-secondary">
+      <ul class="list-group">
+        <li class="list-group-item">Vestuário</li>
+        <li class="list-group-item">Eletrodomésticos</li>
+        <li class="list-group-item">Informática</li>
+        <li class="list-group-item">Ferramentas</li>
+      </ul>
     </div>
-    <div class="">
-      <div class="">
-        <ul>
-          <li>Vestuário</li>
-          <ul>
-            <li><a href="vestuario/camisas">Camisas</a></li>
-            <li><a href="vestuario/calcas">Calças</a></li>
-            <li><a href="vestuario/calcados">Calçados</a></li>
-          </ul>
-          <li>Eletro-eletrônicos</li>
-          <ul>
-            <li><a href="eletroeletronicos/cameras">Câmeras</a></li>
-            <li><a href="eletroeletronicos/desktops">Desktops</a></li>
-            <li><a href="eletroeletronicos/notebooks">Notebooks</a></li>
-            <li><a href="eletroeletronicos/smartphones">Smartphones</a></li>
-            <li><a href="eletroeletronicos/drones">Drones</a></li>
-            <li><a href="eletroeletronicos/fornos">Fornos</a></li>
-            <li><a href="eletroeletronicos/cafeteiras">Cafeteiras</a></li>
-            <li><a href="eletroeletronicos/geladeiras">Geladeiras</a></li>
-          </ul>
-          <li>Ferramentas</li>
-          <ul>
-            <li><a href="ferramentas/marcenaria">Marcenaria</a></li>
-            <li><a href="ferramentas/alvenaria">Alvenaria</a></li>
-            <li><a href="ferramentas/jardinagem">Jardinagem</a></li>
-          </ul>
-          <li>Miscelânia</li>
-          <ul>
-            <li><a href="miscelania/brinquedos">Brinquedos</a></li>
-            <li><a href="miscelania/livros">Livros</a></li>
-            <li><a href="miscelania/decoracao">Decoração</a></li>
-          </ul>
-        </ul>
-      </div>
-      <div class="">
-          <h2>Tela de produtos</h2>
-      </div>
+
+    <!-- TABELA DE PRODUTOS -->
+
+    <div class="col-sm-8 bg-dark">
+
     </div>
+
+
+    <div class="col bg-primary">
+
+    </div>
+  </div>
+</div>
+
 @endsection
--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,7 +1,11 @@
-<nav class="navbar navbar-expand-sm bg-dark fixed-top">
-  <ul class="navbar-nav">
-    <li class="nav-item"> <a class="nav-link" href="#">Link 1</a></li>
-    <li class="nav-item"> <a class="nav-link" href="#">Link 1</a></li>
-    <li class="nav-item"> <a class="nav-link" href="#">Link 1</a></li>
-  </ul>
-</nav>
+<div class="row">
+<div class="col">
+  <nav class="navbar navbar-expand-sm bg-dark fixed-top">
+    <ul class="navbar-nav">
+      <li class="nav-item"> <a class="nav-link" href="#">Link 1</a></li>
+      <li class="nav-item"> <a class="nav-link" href="#">Link 1</a></li>
+      <li class="nav-item"> <a class="nav-link" href="#">Link 1</a></li>
+    </ul>
+  </nav>
+</div>
+</div>
