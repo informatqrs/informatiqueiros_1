9c1c4e1ed6dbfd0dc2065254b6ce4c90fcc91a24
jlucartc <jlucartc@gmail.com>
2018-01-17 09:35:51 -0300

    Jan 17 09:35

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,8 @@
 @extends('master')
-
 @section('conteudo')
 @include('nav')
 <div class="row ">
-<ul class="list-group">
+<ul class="list-group col-sm">
   <a class="list-group-item list-group-item-action ml-2" href="#">Vestuário</a>
   <a class="list-group-item list-group-item-action ml-2" href="#">Eletrodomésticos</a>
   <a class="list-group-item list-group-item-action ml-2" href="#">Informática</a>
@@ -12,10 +11,52 @@
 </ul>
 </nav>
 <div class="col-sm-8 bg-info">
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner" role="listbox">
+      <div class="carousel-item active">
+        <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="First slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Second slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Third slide">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
+  <!-- ITENS -->
+<div class="row justify-content-center mt-4">
+  @for($i = 0; $i < 18; $i++)
+    <div class="card col-3 m-1">
+      <div class="card-img-top">
+        <img class="img-fluid" src="http://placehold.it/400x300" alt="">
+      </div>
+      <div class="card-body">
+        <h5>Produto {{$i}} </h5>
+      </div>
+    </div>
+
+  @endfor
+</div>
 
 </div>
 <div class="col-sm bg-success">
 
 </div>
 </div>
+
 @endsection
--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,10 +1,11 @@
-<nav class="navbar navbar-expand-sm bg-dark sticky-top">
-  <ul class="navbar-nav">
+<nav class="navbar navbar-expand-sm bg-dark sticky-top navbar-toggleable">
+  <a class="navbar-brand" href="#">Marca</a>
+  <ul class="navbar-nav mr-4">
     <li class="nav-item"> <a class="nav-link" href="#">Link 1</a></li>
     <li class="nav-item"> <a class="nav-link" href="#">Link 1</a></li>
     <li class="nav-item"> <a class="nav-link" href="#">Link 1</a></li>
   </ul>
   <form class="form-inline" action="index.html" method="post">
-    <input type="text" name="" value=""><button type="button" name="button"><span class="fa fa-search"></span></button>
+    <input class="form-control mr-1" type="text" name="pesquisa" value="" placeholder="Procure seu produto aqui!"><button class="btn btn-success"type="button" name="button"><span class="fa fa-search"></span></button>
   </form>
 </nav>
